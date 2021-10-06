8537fe6f5e9c1c7091820835016ec862ec970fd9
harsh21p <harshadpachore21@gmail.com>
2021-10-06 11:15:04 +0530

    third page

--- static/js/form.js
+++ static/js/form.js
@@ -1,54 +1,6 @@
 
 
-window.onload = check;
 
-function check() 
-    {
-
-      let a = "{{selectbtn}}";
-
-      if (a == "Passive") {
-        document.getElementById("btn1").focus();
-      }
-      if (a == "Freedrive") {
-        document.getElementById("btn2").focus();
-      }
-      if (a == "Active isotonic") {
-        document.getElementById("btn3").focus();
-      }
-      if (a == "Active isometric") {
-        document.getElementById("btn4").focus();
-      }
-      if (a == "cctive isokinetic") {
-        document.getElementById("btn5").focus();
-      }
-    }
-      
-
-    function passive() {
-
-      document.getElementById("btnselect").value = "Passive";
-    }
-
-    function passive1() {
-
-      document.getElementById("btnselect").value = "Freedrive";
-    }
-
-    function passive2() {
-
-      document.getElementById("btnselect").value = "Active isotonic";
-    }
-
-    function passive3() {
-
-      document.getElementById("btnselect").value = "Active isometric";
-    }
-    function passive4() {
-
-      document.getElementById("btnselect").value = "Acctive isokinetic";
-    }
-  
 
 function myprofile(clr) {
     location.href = clr;
