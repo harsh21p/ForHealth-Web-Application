db239448f7efe1d771ce65795315b3b783bbda78
harsh21p <harshadpachore21@gmail.com>
2021-10-08 11:24:25 +0530

    sidenav

--- static/js/form.js
+++ static/js/form.js
@@ -7,7 +7,7 @@ function myprofile(clr) {
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("mySidenav").style.width = "236px;";
   }
 
   function closeNav() {
