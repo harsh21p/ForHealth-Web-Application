39026d5ec91537697fa8e4df90b36856a203e716
harsh21p <harshadpachore21@gmail.com>
2021-11-12 17:46:44 +0530

    Upload data and download data methods added

--- mongoDB.py
+++ mongoDB.py
@@ -1,42 +1,53 @@
-from pymongo import MongoClient
 import pymongo
+from pymongo import MongoClient
 import sqlite3
 
-# Database connection method ... 
+# Database connection method ...
+
+db = sqlite3.connect("Database.db", check_same_thread=False)
+db.row_factory = sqlite3.Row
+cursor = db.cursor()
+
+CONNECTION_STRING = "mongodb+srv://harshad:harshad@cluster0.9nhrp.mongodb.net/bank?retryWrites=true&w=majority"
+client = MongoClient(CONNECTION_STRING)
 
 def get_database():
-    
-    CONNECTION_STRING = "mongodb+srv://harshad:harshad@cluster0.9nhrp.mongodb.net/bank?retryWrites=true&w=majority"
-    from pymongo import MongoClient
-    client = MongoClient(CONNECTION_STRING)
     return client['forhealth']
 
-# main method ...
+dbname = get_database()
+collection_name = dbname["forhealth"]
 
-if __name__ == "__main__":  
 
-    dbname = get_database()
-
-    collection_name = dbname["forhealth"]
-
-    
+def upload_to_database():
+   
     # Sql connection ...
 
-    db = sqlite3.connect("Database.db", check_same_thread=False)
-    db.row_factory = sqlite3.Row
-    cursor = db.cursor()
-    cursor.execute( "SELECT * FROM info1")
+    cursor.execute("SELECT * FROM info1")
     data = cursor.fetchall()
 
     for i in data:
-        
+
         item = {
-                    "name" : "name_user",
-                    "password" : i['password_user'],
-                   
-                }
+            "name": i["name_user"],
+            "password": i['password_user'],
 
-        # insert data into database 
-     
+        }
+
+        # insert data into database
+
         collection_name.insert_one(item)
+    
 
+
+def download_from_database():
+    for record in collection_name.find().limit(10):
+        print(record)
+        cursor.execute("UPDATE info1 SET uname=(?),uage=(?),uweight=(?),uheight=(?),selectbtn=(?) WHERE name_user=(?)", (record['name_user'], record['uage'], record['uweight'], record['uheight'], record['selectbtn'], record['name_user']))
+    
+
+
+# main method ...
+
+if __name__ == "__main__":
+    upload_to_database()
+    download_from_database()
