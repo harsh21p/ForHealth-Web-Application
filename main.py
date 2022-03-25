be0a34f1c627a308cd3e93fce2430eb56752c050
Harshad Pachore <46632001+harsh21p@users.noreply.github.com>
2022-03-25 16:11:47 +0530

    led off on

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 
 # DEPENDENCIES
 
-from flask import Flask, render_template, request, redirect, session, url_for, make_response
+from flask import Flask,jsonify, render_template, request, redirect, session, url_for, make_response
 from flask import Flask, render_template, request
 import sqlite3
 import json
 from time import time
 from random import random
-# import Adafruit_ADS1x15
+import Adafruit_ADS1x15
 from flask.helpers import flash
+import xlwt
+from xlwt import Workbook
+import numpy as np
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(21, GPIO.OUT) 
+
+def led_on():
+    GPIO.output(21, GPIO.HIGH) 
+
+def led_off():
+    GPIO.output(21, GPIO.LOW)
 
 # FLASK APP NEW
 
@@ -28,6 +43,15 @@ def myhome():
     session['username'] = "null"
     return render_template("home.html")
 
+
+
+@app.route('/on', methods=['GET'])
+def on():
+    led_on()
+
+@app.route('/off', methods=['GET'])
+def off():
+    led_off()
 # LOGIN
 
 
@@ -277,7 +301,7 @@ def data():
             # adc.start_adc(0, gain=GAIN)
             # value = adc.get_last_result()
 
-            data = [time() * 10000, random()*100*time()]
+            data = [time() * 10000, randint(0,20000)]
             response = make_response(json.dumps(data))
             response.content_type = 'application/json'
             return response
--- static/js/getdata.js
+++ static/js/getdata.js
@@ -11,27 +11,28 @@ var l=0;
 
 function requestData() {
 
-    if(i==20){
-        $.ajax({
-            url: '/data1',
-            success: function(point) {
-                var series = chart.series[0],
-                    shift = series.data.length > 20; // shift if the series is
-                                                     // longer than 20
+    // if(i==20){
+    //     $.ajax({
+    //         url: '/data1',
+    //         success: function(point) {
+    //             var series = chart.series[0],
+    //                 shift = series.data.length > 20; // shift if the series is
+    //                                                  // longer than 20
                     
-                // add the point
+    //             // add the point
                 
-                charta.series[0].addPoint(point, true, shift);
+    //             charta.series[0].addPoint(point, true, shift);
                
     
                
-            },
-            cache: false
-        });
-        i=0;
-    }
-
+    //         },
+    //         cache: false
+    //     });
+    //     i=0;
+    // }
+    
     if(j==5){
+        
         $.ajax({
             url: '/data',
             success: function(point) {
@@ -40,6 +41,7 @@ function requestData() {
                                                     // longer than 20
                     
                 // add the point
+                chart.yAxis[0].setExtremes(0,30000);
                 chart.series[0].addPoint(point, true, shift);
             
 
@@ -51,48 +53,48 @@ function requestData() {
 
     }
 
-    if(k==10){
-        $.ajax({
-            url: '/data2',
-            success: function(point) {
-                var series = chart.series[0],
-                    shift = series.data.length > 20; // shift if the series is
-                                                    // longer than 20
+    // if(k==10){
+    //     $.ajax({
+    //         url: '/data2',
+    //         success: function(point) {
+    //             var series = chart.series[0],
+    //                 shift = series.data.length > 20; // shift if the series is
+    //                                                 // longer than 20
                     
-                // add the point
+    //             // add the point
                 
-                chartb.series[0].addPoint(point, true, shift);
+    //             chartb.series[0].addPoint(point, true, shift);
             
 
-                // call it again after one second
+    //             // call it again after one second
                 
-            },
-            cache: false
-        });
-        k=0;
-    }
+    //         },
+    //         cache: false
+    //     });
+    //     k=0;
+    // }
 
    
-    if(l==15){
-        $.ajax({
-            url: '/data3',
-            success: function(point) {
-                var series = chart.series[0],
-                    shift = series.data.length > 20; // shift if the series is
-                                                    // longer than 20
+    // if(l==15){
+    //     $.ajax({
+    //         url: '/data3',
+    //         success: function(point) {
+    //             var series = chart.series[0],
+    //                 shift = series.data.length > 20; // shift if the series is
+    //                                                 // longer than 20
                     
-                // add the point
+    //             // add the point
                 
-                chartc.series[0].addPoint(point, true, shift);
+    //             chartc.series[0].addPoint(point, true, shift);
             
 
-                // call it again after one second
+    //             // call it again after one second
                 
-            },
-            cache: false
-        });
-        l=0;
-    }
+    //         },
+    //         cache: false
+    //     });
+    //     l=0;
+    // }
 
     i=i+1;
     j=j+1;
@@ -154,148 +156,150 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function() {
-    charta = new Highcharts.Chart({
-        chart: {
-            renderTo: 'data-container1',
-            defaultSeriesType: 'spline',
-            events: {
-                load: requestData
-            }
-        },
-        title: {
-            text: 'Live2'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150,
-            maxZoom: 20 * 1000
-        },
-        yAxis: {
-            minPadding: 0.2,
-            maxPadding: 0.2,
-            title: {
-                text: 'Value',
-                margin: 10
-            }
-        },
-        plotOptions: {
+// $(document).ready(function() {
+//     charta = new Highcharts.Chart({
+//         chart: {
+//             renderTo: 'data-container1',
+//             defaultSeriesType: 'spline',
+//             events: {
+//                 load: requestData
+//             }
+//         },
+//         title: {
+//             text: 'Live2'
+//         },
+//         xAxis: {
+//             type: 'datetime',
+//             tickPixelInterval: 150,
+//             maxZoom: 20 * 1000
+//         },
+//         yAxis: {
+//             min:0,
+//             max:10,
+//             minPadding: 0.2,
+//             maxPadding: 0.2,
+//             title: {
+//                 text: 'Value',
+//                 margin: 10
+//             }
+//         },
+//         plotOptions: {
             
-            series: {
-                color:'hsl(18, 84%, 59%)',
-                allowPointSelect: true,
-                marker: {
-                    fillColor: 'hsl(18, 84%, 59%)',
-                    states: {
-                        select: {
-                            fillColor: 'red',
-                            lineWidth: 0
-                        }
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Data',
-            data: []
-        }]
-    });
-});
+//             series: {
+//                 color:'hsl(18, 84%, 59%)',
+//                 allowPointSelect: true,
+//                 marker: {
+//                     fillColor: 'hsl(18, 84%, 59%)',
+//                     states: {
+//                         select: {
+//                             fillColor: 'red',
+//                             lineWidth: 0
+//                         }
+//                     }
+//                 }
+//             }
+//         },
+//         series: [{
+//             name: 'Data',
+//             data: []
+//         }]
+//     });
+// });
 
-$(document).ready(function() {
-    chartb = new Highcharts.Chart({
-        chart: {
-            renderTo: 'data-container2',
-            defaultSeriesType: 'spline',
-            events: {
-                load: requestData
-            }
-        },
-        title: {
-            text: 'Live3'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150,
-            maxZoom: 20 * 1000
-        },
-        yAxis: {
-            minPadding: 0.2,
-            maxPadding: 0.2,
-            title: {
-                text: 'Value',
-                margin: 0
-            }
-        },
-        plotOptions: {
+// $(document).ready(function() {
+//     chartb = new Highcharts.Chart({
+//         chart: {
+//             renderTo: 'data-container2',
+//             defaultSeriesType: 'spline',
+//             events: {
+//                 load: requestData
+//             }
+//         },
+//         title: {
+//             text: 'Live3'
+//         },
+//         xAxis: {
+//             type: 'datetime',
+//             tickPixelInterval: 150,
+//             maxZoom: 20 * 1000
+//         },
+//         yAxis: {
+//             minPadding: 0.2,
+//             maxPadding: 0.2,
+//             title: {
+//                 text: 'Value',
+//                 margin: 0
+//             }
+//         },
+//         plotOptions: {
             
-            series: {
-                color:'hsl(18, 84%, 59%)',
-                allowPointSelect: true,
-                marker: {
-                    fillColor: 'hsl(18, 84%, 59%)',
-                    states: {
-                        select: {
-                            fillColor: 'red',
-                            lineWidth: 0
-                        }
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Data',
-            data: []
-        }]
-    });
-});
+//             series: {
+//                 color:'hsl(18, 84%, 59%)',
+//                 allowPointSelect: true,
+//                 marker: {
+//                     fillColor: 'hsl(18, 84%, 59%)',
+//                     states: {
+//                         select: {
+//                             fillColor: 'red',
+//                             lineWidth: 0
+//                         }
+//                     }
+//                 }
+//             }
+//         },
+//         series: [{
+//             name: 'Data',
+//             data: []
+//         }]
+//     });
+// });
 
 
-$(document).ready(function() {
-    chartc = new Highcharts.Chart({
-        chart: {
-            renderTo: 'data-container3',
-            defaultSeriesType: 'spline',
-            events: {
-                load: requestData
-            }
-        },
-        title: {
-            text: 'Live4'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 50,
-            maxZoom: 20 * 1000
-        },
-        yAxis: {
-            minPadding: 0.2,
-            maxPadding: 0.2,
-            title: {
-                text: 'Value',
-                margin: 0,
-            }
-        },
-        plotOptions: {
+// $(document).ready(function() {
+//     chartc = new Highcharts.Chart({
+//         chart: {
+//             renderTo: 'data-container3',
+//             defaultSeriesType: 'spline',
+//             events: {
+//                 load: requestData
+//             }
+//         },
+//         title: {
+//             text: 'Live4'
+//         },
+//         xAxis: {
+//             type: 'datetime',
+//             tickPixelInterval: 50,
+//             maxZoom: 20 * 1000
+//         },
+//         yAxis: {
+//             minPadding: 0.2,
+//             maxPadding: 0.2,
+//             title: {
+//                 text: 'Value',
+//                 margin: 0,
+//             }
+//         },
+//         plotOptions: {
             
-            series: {
-                color:'hsl(18, 84%, 59%)',
-                allowPointSelect: true,
-                marker: {
-                    fillColor: 'hsl(18, 84%, 59%)',
-                    states: {
-                        select: {
-                            fillColor: 'red',
-                            lineWidth: 0
-                        }
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Data',
-            data: []
-        }]
-    });
-});
+//             series: {
+//                 color:'hsl(18, 84%, 59%)',
+//                 allowPointSelect: true,
+//                 marker: {
+//                     fillColor: 'hsl(18, 84%, 59%)',
+//                     states: {
+//                         select: {
+//                             fillColor: 'red',
+//                             lineWidth: 0
+//                         }
+//                     }
+//                 }
+//             }
+//         },
+//         series: [{
+//             name: 'Data',
+//             data: []
+//         }]
+//     });
+// });
 
